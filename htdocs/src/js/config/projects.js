fdd1ffe2f3aac68d7520f8ece4334ef32bb2ac4a
Armand Philippot <git@armandphilippot.com>
2021-10-24 17:35:12 +0200

    chore: define projects list

---
+++ htdocs/src/js/config/projects.js
@@ -0,0 +1,77 @@
+const projects = [
+  {
+    id: 'bin2dec',
+    name: 'Bin2Dec',
+    description: 'Convert a binary string to a decimal number.',
+    path: './projects/js-small-apps/bin2dec/index.html',
+    repo: [
+      {
+        name: 'Github',
+        url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/bin2dec',
+      },
+      {
+        name: 'Gitlab',
+        url: '#',
+      },
+    ],
+    technologies: ['Vanilla Javascript'],
+  },
+  {
+    id: 'budget-app',
+    name: 'Budget App',
+    path: './projects/js-small-apps/budget-app/index.html',
+    repo: [
+      {
+        name: 'Github',
+        url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/budget-app',
+      },
+    ],
+  },
+  {
+    id: 'calculator',
+    name: 'Calculator',
+    path: './projects/js-small-apps/calculator/index.html',
+    repo: [
+      {
+        name: 'Github',
+        url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/calculator',
+      },
+    ],
+  },
+  {
+    id: 'clock',
+    name: 'Clock',
+    path: './projects/js-small-apps/clock/index.html',
+    repo: [
+      {
+        name: 'Github',
+        url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/clock',
+      },
+    ],
+  },
+  {
+    id: 'css-border-previewer',
+    name: 'CSS Border Previewer',
+    path: './projects/js-small-apps/css-border-previewer/index.html',
+    repo: [
+      {
+        name: 'Github',
+        url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/css-border-previewer',
+      },
+    ],
+  },
+  {
+    id: 'rps-game',
+    name: 'Rock Paper Scissors',
+    path: './projects/js-small-apps/rock-paper-scissors/index.html',
+    repo: [
+      {
+        name: 'Github',
+        url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/rock-paper-scissors',
+      },
+    ],
+    technologies: ['Vanilla Javascript'],
+  },
+];
+
+export default projects;
