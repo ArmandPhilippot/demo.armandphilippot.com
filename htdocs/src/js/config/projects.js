50969aeafec3582b10c30b484169e9818939837a
Armand Philippot <git@armandphilippot.com>
2021-10-26 16:59:35 +0200

    chore: add description and technologies for each projets

--- htdocs/src/js/config/projects.js
+++ htdocs/src/js/config/projects.js
@@ -9,16 +9,14 @@ const projects = [
         name: 'Github',
         url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/bin2dec',
       },
-      {
-        name: 'Gitlab',
-        url: '#',
-      },
     ],
     technologies: ['Vanilla Javascript'],
   },
   {
     id: 'budget-app',
     name: 'Budget App',
+    description:
+      'By selecting a language in the initialization form, only the currency is converted (the app is not translated). Also, no data is saved on reload.',
     path: './projects/js-small-apps/budget-app/index.html',
     repo: [
       {
@@ -26,10 +24,12 @@ const projects = [
         url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/budget-app',
       },
     ],
+    technologies: ['Vanilla Javascript'],
   },
   {
     id: 'calculator',
     name: 'Calculator',
+    description: 'A basic calculator.',
     path: './projects/js-small-apps/calculator/index.html',
     repo: [
       {
@@ -37,10 +37,13 @@ const projects = [
         url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/calculator',
       },
     ],
+    technologies: ['Vanilla Javascript'],
   },
   {
     id: 'clock',
     name: 'Clock',
+    description:
+      'What time is it? You can have the current hour in three formats: an analogic clock, a numeric display or a text.',
     path: './projects/js-small-apps/clock/index.html',
     repo: [
       {
@@ -48,10 +51,13 @@ const projects = [
         url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/clock',
       },
     ],
+    technologies: ['Vanilla Javascript', 'SVG'],
   },
   {
     id: 'css-border-previewer',
     name: 'CSS Border Previewer',
+    description:
+      'Play with CSS borders (style, width, border-radius). Then, you can copy the generated code if the preview suits you.',
     path: './projects/js-small-apps/css-border-previewer/index.html',
     repo: [
       {
@@ -59,10 +65,13 @@ const projects = [
         url: 'https://github.com/ArmandPhilippot/js-small-apps/tree/main/css-border-previewer',
       },
     ],
+    technologies: ['Vanilla Javascript'],
   },
   {
     id: 'rps-game',
     name: 'Rock Paper Scissors',
+    description:
+      'A basic implementation of the game. Try to beat your friend or the computer.',
     path: './projects/js-small-apps/rock-paper-scissors/index.html',
     repo: [
       {
