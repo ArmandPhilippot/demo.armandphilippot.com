4e7d48b96d900119af3dcacd15a97f36a9d693af
Armand Philippot <git@armandphilippot.com>
2021-10-27 15:58:44 +0200

    build(eslint): update rules to avoid conflicts with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,19 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'arrow-body-style': ['error', 'as-needed'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        exports: 'always-multiline',
+        imports: 'always-multiline',
+        objects: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'function-paren-newline': ['error', 'multiline-arguments'],
+    'implicit-arrow-linebreak': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'object-curly-newline': 'off',
     'operator-linebreak': [
