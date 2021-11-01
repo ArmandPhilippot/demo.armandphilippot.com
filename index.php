250ee0012e06b453005556f16a2edeccf11ea913
Armand Philippot <git@armandphilippot.com>
2021-11-01 16:05:49 +0100

    chore: add matomo tracker

--- index.php
+++ index.php
@@ -86,6 +86,28 @@ $dap_current_env = dap_get_current_env();
     </footer>
     <script src="./assets/js/runtime.js"></script>
     <script src="./assets/js/app.js"></script>
+    <!-- Matomo -->
+    <script>
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["setDoNotTrack", true]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u = "//demo.armandphilippot.com/";
+        _paq.push(["setTrackerUrl", u + "config/matomo/matomo.php"]);
+        _paq.push(["setSiteId", "3"]);
+        var d = document,
+          g = d.createElement("script"),
+          s = d.getElementsByTagName("script")[0];
+        g.type = "text/javascript";
+        g.async = true;
+        g.defer = true;
+        g.src = u + "config/matomo/matomo.php";
+        s.parentNode.insertBefore(g, s);
+      })();
+    </script>
+    <!-- End Matomo Code -->
   </body>
 
 </html>
